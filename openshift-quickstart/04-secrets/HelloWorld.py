4f46e836d3825646225ecbeaee733c1e1bfc5334
ly2xxx <ly2xxx@hotmail.com>
2026-03-26 00:39:27 +0000

    remove irrelavant files

--- openshift-quickstart/04-secrets/HelloWorld.py
+++
@@ -1,9 +0,0 @@
-class HelloWorld:
-    """A simple class to print Hello World."""
-    
-    @staticmethod
-    def main():
-        print("Hello, World!")
-
-if __name__ == "__main__":
-    HelloWorld.main()
